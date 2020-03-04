ece085bd9b9d85e330f2c07194351dc3ac3ef59a
jiaxiangzhang <2208638304@qq.com>
2020-03-04 22:34:31 +0800

    webcode

---
+++ web_test.java
@@ -0,0 +1,16 @@
+public class web_test {
+    public static void main(String[] args) {
+        for (int i = 1;i<=200;i++)
+            System.out.println(Fibonacci.of(i));
+    }
+}
+class Fibonacci{
+    public static int of(int num) {
+        if(num ==1||num == 2) {
+            return 1;
+        }else {
+            return of(num-1)+of(num-2);
+        }
+    }
+}
+
